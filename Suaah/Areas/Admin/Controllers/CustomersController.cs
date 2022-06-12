425a49d827308f6503596673d9be8e2b8c4cc8dc
HAZEM-LAB <hazm.popo@gmail.com>
2022-06-12 23:41:19 +0200

    fix customers controller errors

--- Suaah/Areas/Admin/Controllers/CustomersController.cs
+++ Suaah/Areas/Admin/Controllers/CustomersController.cs
@@ -1,15 +1,10 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Suaah.Data;
-using Suaah.Models;
 
 namespace Suaah.Areas.Admin.Controllers
 {
