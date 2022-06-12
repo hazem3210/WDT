c50d4a4e72c8343059d921dc0401374e8fb774e7
HAZEM-LAB <baviwe4603@falkyz.com>
2022-06-12 09:23:03 +0200

    no

--- Suaah/Models/ErrorViewModel.cs
+++ Suaah/Models/ErrorViewModel.cs
@@ -1,7 +1,7 @@
 namespace Suaah.Models
 {
     public class ErrorViewModel
-    {
+    { 
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
